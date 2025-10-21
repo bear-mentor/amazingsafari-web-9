7eef7212064d2ee154883af688ee541a4576f360
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-21 21:38:06 +0700

    feat: setup layout main with navbar and footer

---
+++ app/layouts/layout-main.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router";
+
+export default function LayoutMain() {
+  const year = new Date().getFullYear();
+
+  return (
+    <div>
+      <nav>
+        <img src="/logo.svg" alt="Logo" />
+      </nav>
+
+      <Outlet />
+
+      <footer>
+        <p>&copy; {year} Amazing Safari</p>
+      </footer>
+    </div>
+  );
+}
