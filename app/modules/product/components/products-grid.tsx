35ad8dc85e9189871f228cad602412f8c54f0ea1
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-21 22:10:43 +0700

    feat: link product slug

--- app/modules/product/components/products-grid.tsx
+++ app/modules/product/components/products-grid.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { formatPrice } from "~/lib/format";
 import type { Products } from "~/modules/product/type";
@@ -8,26 +9,28 @@ export function ProductsGrid({ products }: { products: Products }) {
       {products.map((product) => {
         return (
           <li key={product.id}>
-            <Card className="overflow-hidden">
-              <div className="aspect-square overflow-hidden">
-                <img
-                  src={product.imageUrl}
-                  alt={product.name}
-                  className="w-full h-full object-cover"
-                />
-              </div>
-              <CardHeader>
-                <CardTitle className="text-lg">{product.name}</CardTitle>
-              </CardHeader>
-              <CardContent className="flex flex-col items-end">
-                <p className="text-2xl font-bold text-primary-foreground">
-                  {formatPrice(product.price)}
-                </p>
-                <p className="text-md text-muted-foreground">
-                  Stock: {product.stock}
-                </p>
-              </CardContent>
-            </Card>
+            <Link to={`/products/${product.slug}`}>
+              <Card className="overflow-hidden">
+                <div className="aspect-square overflow-hidden">
+                  <img
+                    src={product.imageUrl}
+                    alt={product.name}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <CardHeader>
+                  <CardTitle className="text-lg">{product.name}</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col items-end">
+                  <p className="text-2xl font-bold text-primary-foreground">
+                    {formatPrice(product.price)}
+                  </p>
+                  <p className="text-md text-muted-foreground">
+                    Stock: {product.stock}
+                  </p>
+                </CardContent>
+              </Card>
+            </Link>
           </li>
         );
       })}
